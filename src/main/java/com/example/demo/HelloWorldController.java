6dc9d52034d10116ee759883e477f3ab37886089
Lingxiao Huang <jasonhlx1213@gmail.com>
2022-05-22 16:05:56 +0300

    change

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -3,14 +3,15 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
     // need a controller method to show the initial HTML form
-
     @RequestMapping("/showForm")
     public String showForm() {
         return "helloworld-form";
@@ -35,6 +36,21 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all uppercase
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey My Friend ! " + theName;
 
         // add message to the model
         model.addAttribute("message", result);
